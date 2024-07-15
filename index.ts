f7097f8d8240339a89484f710f7b0cd0f0757833
Antonio Stoilkov <hello@astoilkov.com>
2024-07-15 11:24:49 +0300

    🏗 add default export

---
+++ index.ts
@@ -0,0 +1,8 @@
+import useStorageState, {
+    type StorageState,
+    type StorageStateOptions,
+} from './src/useStorageState.js'
+
+export default useStorageState
+
+export type { StorageState, StorageStateOptions }
