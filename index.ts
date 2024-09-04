8c3fb6ea27eea528b203e5cf43734d757fd5d8ce
Antonio Stoilkov <hello@astoilkov.com>
2024-09-04 11:19:06 +0300

    🎨 prettier

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import useStorageState, {
     type StorageState,
     type StorageStateOptions,
-} from './src/useStorageState.js'
-import memoryStorage from './src/memoryStorage.js'
+} from "./src/useStorageState.js";
+import memoryStorage from "./src/memoryStorage.js";
 
-export default useStorageState
+export default useStorageState;
 
-export { memoryStorage }
+export { memoryStorage };
 
-export type { StorageState, StorageStateOptions }
+export type { StorageState, StorageStateOptions };
