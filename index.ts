0aa6f6e41a82adb9573ce3836acef5ff6169532f
Antonio Stoilkov <hello@astoilkov.com>
2024-09-04 11:18:49 +0300

    ✨ export `memoryStorage`

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import useStorageState, {
     type StorageState,
     type StorageStateOptions,
 } from './src/useStorageState.js'
+import memoryStorage from './src/memoryStorage.js'
 
 export default useStorageState
+
+export { memoryStorage }
 
 export type { StorageState, StorageStateOptions }
